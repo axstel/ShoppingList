a2b52d2ba22330b8b92f747fcb92e3cb6f190228
Spaten <Spaten@Spaten-PC>
2011-11-24 19:39:07 +0100

    test

--- src/com/wbh/loewe/shoppinglist/ShoppingListActivity.java
+++ src/com/wbh/loewe/shoppinglist/ShoppingListActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 public class ShoppingListActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) { //Hallo
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
