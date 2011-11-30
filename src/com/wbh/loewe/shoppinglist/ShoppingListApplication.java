17df8c0ca4cbf039ac2474dea6f2bfa559310e21
Alexander.Stark <alexander.stark@cas.de>
2011-11-30 08:33:01 +0100

    Klasse Application hinzugefügt um später die Datenbankverbindung zu verwalten

---
+++ src/com/wbh/loewe/shoppinglist/ShoppingListApplication.java
@@ -0,0 +1,33 @@
+package com.wbh.loewe.shoppinglist;
+
+import android.app.Application;
+
+public class ShoppingListApplication extends Application {
+	
+	//private ShoppingListDatabaseAdapter mDBAdapter;
+	
+	@Override
+    public void onCreate() {
+        super.onCreate();
+        /*
+        mDBAdapter = new ShoppingListDatabaseAdapter(getApplicationContext());
+        mDBAdapter.open();
+        */
+    }
+
+	@Override
+	public void onTerminate() {
+		/*
+		if (mDBAdapter != null) {
+			mDBAdapter.close();
+		}
+		*/
+		super.onTerminate();
+	}
+	
+	/*
+	public ShoppingListDatabaseAdapter getDBAdapter() {
+		return mDBAdapter;
+	}
+	*/
+}
