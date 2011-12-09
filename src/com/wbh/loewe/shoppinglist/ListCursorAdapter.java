a32cff48309da3a39866eb0095a1d95c9309e033
Alexander.Stark <alexander.stark@cas.de>
2011-12-09 08:57:59 +0100

    Versuch das OnListItemClick zu implementieren

--- src/com/wbh/loewe/shoppinglist/ListCursorAdapter.java
+++ src/com/wbh/loewe/shoppinglist/ListCursorAdapter.java
@@ -5,11 +5,11 @@ import java.util.HashMap;
 import android.content.Context;
 import android.database.Cursor;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
 
@@ -36,10 +36,10 @@ public class ListCursorAdapter extends SimpleCursorAdapter {
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {	
 		
+		View lView = super.getView(position, convertView, parent);
+		
 		mCursor.moveToPosition(position);
-		
 		//Log.w(ListCursorAdapter.class.getName(), "getView "+ mCursor.getCount() +" "+ mCursor.getColumnCount() +" "+ mCursor.getColumnCount());
-		
 		int lID = mCursor.getInt(mCursor.getColumnIndex(ShoppingListDatabase.FIELD_NAME_ID));
 		ListItem lListItem;
 		if (mItems.containsKey(lID)) {
@@ -48,33 +48,35 @@ public class ListCursorAdapter extends SimpleCursorAdapter {
 			lListItem = new ListItem(lID, mCursor.getString(mCursor.getColumnIndex(ShoppingListDatabase.FIELD_NAME_NAME)));
 			mItems.put(lID, lListItem);
 		}
-			
-		View view = null;
-		if (convertView == null) {
-			LayoutInflater inflator = LayoutInflater.from(mContext);
-			view = inflator.inflate(R.layout.list_row, null);
-			final ViewHolder viewHolder = new ViewHolder();
-			viewHolder.mID = (TextView) view.findViewById(R.id.labelid);
-			viewHolder.mText = (TextView) view.findViewById(R.id.labelname);
-			viewHolder.mEdit = (Button) view.findViewById(R.id.btn_edititem);
-			viewHolder.mEdit
-					.setOnClickListener(new OnClickListener() {
+		
+		final ViewHolder lViewHolder = (ViewHolder)lView.getTag();
+		if (lViewHolder == null) {
+			final ViewHolder lNewViewHolder = new ViewHolder();
+			lNewViewHolder.mID = (TextView) lView.findViewById(R.id.labelid);
+			lNewViewHolder.mText = (TextView) lView.findViewById(R.id.labelname);
+			lNewViewHolder.mEdit = (Button) lView.findViewById(R.id.btn_edititem);
+			lNewViewHolder.mEdit.setOnClickListener(new OnClickListener() {
 					    public void onClick(View v) {
-					    	ListItem lItem = (ListItem) viewHolder.mEdit.getTag();
+					    	ListItem lItem = (ListItem) lNewViewHolder.mEdit.getTag();
 					    	lItem.setSelected(!lItem.getSelected());
 					    	Log.w(ListCursorAdapter.class.getName(), "OnEditClick "+ lItem.getID() +" "+ lItem.getName());
 					    }
 					});
-			view.setTag(viewHolder);
-			viewHolder.mEdit.setTag(lListItem);
+			lView.setTag(lNewViewHolder);
+			lNewViewHolder.mEdit.setTag(lListItem);
 		} else {
-			view = convertView;
-			((ViewHolder) view.getTag()).mEdit.setTag(lListItem);
+			((ViewHolder)lView.getTag()).mEdit.setTag(lListItem);
 		}
-		ViewHolder holder = (ViewHolder) view.getTag();
-		holder.mID.setText(Integer.toString(lListItem.getID()));
-		holder.mText.setText(lListItem.getName());
+
+		ViewHolder lHolder = (ViewHolder)lView.getTag();
+		lHolder.mID.setText(Integer.toString(lListItem.getID()));
+		lHolder.mText.setText(lListItem.getName());
 		
-		return view;
+		return lView;
 	}
+	
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		Log.w(ListCursorAdapter.class.getName(), "onListItemClick");
+	}
+
 }
